87ef19ffd9201f920cdc73c4ec0cb2f2245ae49b
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-13 02:13:27 +0530

    log adapter added

--- app/src/main/java/pranav/apps/amazing/debouncesearch/MainActivity.java
+++ app/src/main/java/pranav/apps/amazing/debouncesearch/MainActivity.java
@@ -1,13 +1,29 @@
 package pranav.apps.amazing.debouncesearch;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.subscribers.DisposableSubscriber;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public class LogAdapter extends ArrayAdapter<String>{
+
+        public LogAdapter(Context context, List<String> logs) {
+            super(context, R.layout.item_log,R.id.item_log,logs);
+        }
     }
 }
