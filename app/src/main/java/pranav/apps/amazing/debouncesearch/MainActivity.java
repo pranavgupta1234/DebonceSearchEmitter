92136e75eeef1c2a6d8e4c2f1d6997c3e378d375
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-13 14:21:15 +0530

    interop library for rx binding widgets added

--- app/src/main/java/pranav/apps/amazing/debouncesearch/MainActivity.java
+++ app/src/main/java/pranav/apps/amazing/debouncesearch/MainActivity.java
@@ -4,19 +4,46 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.jakewharton.rxbinding.widget.RxTextView;
 
 import java.util.List;
 
+import io.reactivex.Flowable;
 import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.subscribers.DisposableSubscriber;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LogAdapter _logAdapter;
+
+    private EditText input;
+    private ListView _logView;
+
+
+
+    //Flowable is much like an observable available in rx 2.x
+    private Flowable<CharSequence> _inputTextSearch;
+
+    //Disposable Subscriber is a type of observer available in 2.x version
+    DisposableSubscriber<Boolean> _disposable;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (EditText)findViewById(R.id.input_txt_debounce);
+        _logView = (ListView)findViewById(R.id.list_threading_log);
+
+        _inputTextSearch = RxTextView.textChanges(input);
+
+
+
     }
 
 
