0f6397a9620475966af26e9c87df864e3b227b7e
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-13 21:00:07 +0530

    app is functional

--- app/src/main/java/pranav/apps/amazing/debouncesearch/MainActivity.java
+++ app/src/main/java/pranav/apps/amazing/debouncesearch/MainActivity.java
@@ -1,34 +1,53 @@
 package pranav.apps.amazing.debouncesearch;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.jakewharton.rxbinding.widget.RxTextView;
+import com.jakewharton.rxbinding.widget.TextViewAfterTextChangeEvent;
+import com.jakewharton.rxbinding.widget.TextViewTextChangeEvent;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import hu.akarnokd.rxjava.interop.RxJavaInterop;
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.subscribers.DisposableSubscriber;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     private LogAdapter _logAdapter;
 
     private EditText input;
-    private ListView _logView;
 
+    private ListView _logView;
+    private List<CharSequence> _logs;
+
+    private ImageButton clr;
 
 
     //Flowable is much like an observable available in rx 2.x
-    private Flowable<CharSequence> _inputTextSearch;
+    //private Observable<CharSequence> _inputTextSearch;
 
     //Disposable Subscriber is a type of observer available in 2.x version
-    DisposableSubscriber<Boolean> _disposable;
+    //DisposableSubscriber<Boolean> _disposable;
+
+    private Disposable _disposable;
 
 
 
@@ -39,13 +58,97 @@ public class MainActivity extends AppCompatActivity {
 
         input = (EditText)findViewById(R.id.input_txt_debounce);
         _logView = (ListView)findViewById(R.id.list_threading_log);
+        clr=(ImageButton)findViewById(R.id.clr_debounce);
+        _setupLogger();
+        //using interop library we made an instance of v2 flowable from rx binding which supports v1 only :(
+        //_inputTextSearch = RxJavaInterop.toV2Observable(RxTextView.textChanges(input));
 
-        _inputTextSearch = RxTextView.textChanges(input);
+        //Here we will schedule the observable to observer
+        //in textChangeEvents value is emitted immediately on subscribe
+        _disposable = RxJavaInterop.toV2Observable(RxTextView.textChangeEvents(input))
+                .debounce(400, TimeUnit.MILLISECONDS)// default Scheduler is Computation
+                .filter(changes -> isNotNullOrEmpty(input.getText().toString()))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(_getObserver());
+        clr.setOnClickListener(view -> {
+            _logs = new ArrayList<>();
+            _logAdapter.clear();
+        });
 
 
 
     }
 
+    private boolean isNotNullOrEmpty(String s) {
+        if(s!= null){
+            return true;
+        }
+        else
+            return false;
+    }
+    //method returning an observer
+
+    private DisposableObserver<TextViewTextChangeEvent> _getObserver(){
+
+        return new DisposableObserver<TextViewTextChangeEvent>() {
+            @Override
+            /*As our observable is an observable to CharSequence and
+              observer is a disposable subscriber to  textchange so will receive similar thing as argument
+            * */
+            public void onNext(TextViewTextChangeEvent textViewTextChangeEvent) {
+                _log("Searching for "+ textViewTextChangeEvent.text().toString()+"\n");
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Timber.d("Oops an error occur");
+                _log("Oops there is an error");
+            }
+
+            @Override
+            public void onComplete() {
+                Timber.d("........complete");
+            }
+        };
+
+    }
+
+    private void _setupLogger() {
+        _logs = new ArrayList<>();
+        _logAdapter = new LogAdapter(MainActivity.this, new ArrayList<>());
+        _logView.setAdapter(_logAdapter);
+    }
+
+    private void _log(String logMsg) {
+
+        if (_isCurrentlyOnMainThread()) {
+            _logs.add(0, logMsg + " (main thread) ");
+            _logAdapter.clear();
+            _logAdapter.addAll(_logs.toString());
+        } else {
+            _logs.add(0, logMsg + " (NOT main thread) ");
+
+            // You can only do below stuff on main thread.
+            new Handler(Looper.getMainLooper()).post(() -> {
+                _logAdapter.clear();
+                _logAdapter.addAll(_logs.toString());
+            });
+        }
+    }
+
+    private boolean _isCurrentlyOnMainThread() {
+        return Looper.myLooper() == Looper.getMainLooper();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(!_disposable.isDisposed()){
+            _disposable.dispose();
+        }
+    }
 
     public class LogAdapter extends ArrayAdapter<String>{
 
